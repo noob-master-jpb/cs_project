338f0841e362de50be511f62cb1518906d0daa71
Jyotipriya <b8777051595@gmail.com>
2023-11-12 01:47:34 +0530

    basic inventory window

--- Inventory_window.py
+++ Inventory_window.py
@@ -12,12 +12,32 @@ def show_all_inventory(rec = view_table(tab),table = tab):
 show_all_inventory()
 print("1. To USE FILTER")
 print("2. To USE SORTING")
-at = int(input("---> "))
+at = int(input("--> "))
 if at == 1:
-    print("1. by NAME")
-    print("2. by COST PRICE")
-    print("3. by SELL PRICE")
-    print("4. by DATE ADDED")
-    print("5. by EXPIRY DATE")
+    print("1. by COST PRICE")
+    print("2. by SELL PRICE")
+    print("3. by DATE ADDED")
+    print("4. by EXPIRY DATE")
+    ap = int(input("--> "))
+    if ap == 1:
+        print("PUT THE RANGE OF COST PRICE")
+        up = int(input("FROM--> "))
+        down = int(input("TO--> "))
+        print(up,down)
 
+    elif ap == 2:
+        print("PUT THE RANGE OF SELL PRICE")
+        up = int(input("FROM--> "))
+        down = int(input("TO--> "))
+        print(up, down)
+    elif ap == 3:
+        print("PUT THE RANGE OF DATE ADDED")
+        up = int(input("FROM--> "))
+        down = int(input("TO--> "))
+        print(up, down)
+    elif ap == 3:
+        print("PUT THE RANGE OF EXPIRY DATE")
+        up = int(input("FROM--> "))
+        down = int(input("TO--> "))
+        print(up, down)
 
