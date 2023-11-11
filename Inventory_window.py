cef4402f4eee94dd475f7a22116ae66f48a02e39
Jyotipriya <b8777051595@gmail.com>
2023-11-12 00:53:46 +0530

    fixed some functions and added some more

--- Inventory_window.py
+++ Inventory_window.py
@@ -2,13 +2,12 @@ from function_set import *
 from database import *
 
 tab = 'INVENTORY'
-
-t = align_records_with_head(view_table(tab), tab)
-
-for i in range(0, len(t)):
-    for j in t:
-        print(j[i], end='| ')
-    print()
-
-
-
+print("ALL RECORDS")
+def show_all_inventory(rec = view_table(tab),table = tab):
+    t = align_records_with_head(rec, table)
+    for i in range(0, len(t)):
+        for j in t:
+            print(j[i], end='| ')
+        print()
+show_all_inventory()
+print("1. To USE ")
