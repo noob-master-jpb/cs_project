a8ac6968f98ad3dafc3e3d6319fa979fd0a56349
Jyotipriya <b8777051595@gmail.com>
2023-11-12 21:46:01 +0530

    basic inventory window

--- Inventory_window.py
+++ Inventory_window.py
@@ -11,11 +11,17 @@ def show_all_inventory(rec=view_table(tab), table=tab):
         for j in t:
             print(j[i], end='| ')
         print()
-
+    return t
 
 header = ret_header(tab)
 
-def filter_inventory(tab = "INVENTORY"):
+tt = show_all_inventory()
+
+print(tt)
+# def sort(lis):
+#     print()
+
+def filter_inventory(tab="INVENTORY"):
     ls = ['COST_PRICE', "SELL_PRICE", "QUANTITY", "DATE_ADDED", "EXPIRY"]
     for i in enumerate(ls):
         print(f"{i[0] + 1}. by {i[1].replace('_', ' ')}")
@@ -31,27 +37,26 @@ def filter_inventory(tab = "INVENTORY"):
     filt_data = control.fetchall()
     show_all_inventory(filt_data, tab)
     return filt_data
-show_all_inventory()
-ctrl = 0
-rec_state = 0
-temp_rec = None
-while not ctrl:
-    print("1. To USE FILTER")
-    print("2. To USE SORTING")
 
-    at = int(input("--> "))
-    if at == 1:
-        if not rec_state:
-            temp_rec = filter_inventory()
-            rec_state = 1
-        else:
-            print("1. filter from this table".upper())
-            print("2. filter from full table".upper())
 
-    elif at == 2:
-        for i in enumerate(header):
-            print(f"{i[0] + 1}. by {i[1].replace('_', ' ')}")
-        srt = int(input("-->"))
-        print("1. ascending".upper())
-        print("2. Descending".upper())
-        srt_type = int(input("-->"))
+
+# ctrl = 0
+# while not ctrl:
+#     print("1. To USE FILTER")
+#     print("2. To USE SORTING")
+#     print("3. To CLOSE")
+#
+#     at = int(input("--> "))
+#     if at == 1:
+#         temp_data = filter_inventory()
+#         print("DO YOU WANT TO SORT THIS TABLE? y/n")
+#         sr = input("-->")
+#         # if sr.upper() == 'Y':
+#
+#     elif at == 2:
+#         for i in enumerate(header):
+#             print(f"{i[0] + 1}. by {i[1].replace('_', ' ')}")
+#         srt = int(input("-->"))
+#         print("1. ascending".upper())
+#         print("2. Descending".upper())
+#         srt_type = int(input("-->"))
