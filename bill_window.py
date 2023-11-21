14e036e25f0770c9fe29828560b0976ac3864723
AYUSH 'n' AHANA <dasayush322@proton.me>
2023-11-21 15:00:00 +0530

    bill function

--- bill_window.py
+++ bill_window.py
@@ -1,52 +1,11 @@
-# class Item:
-#     def __init__(self, item_id, name, price):
-#         self.id = item_id
-#         self.name = name
-#         self.price = price
-#
-#
-#
-# def take_customer_info():
-#     name = input('Name: ')
-#     address = input('Address: ')
-#     return name, address
-#
-#
-#
-#
-# #ctrl = 0
-# #while by ctrl
-#
-# def take_item_list():
-#     total_items = int(input('Enter the total number of items: '))
-#     items = []
-#     for i in range(total_items):
-#         item_id = i + 1
-#         name = input('Enter item name: ')
-#         price = int(input('Enter price: '))
-#         items.append(Item(item_id, name, price))
-#     return items
-#
-#
-# def display_bill(items, customer_name, customer_address):
-#     total = 0
-#     print('\n\n\n')
-#     print('\t       Store       ')
-#     print('\t -----------------')
-#     print(f"Name : {customer_name} \t Address : {customer_address}")
-#     for obj in items:
-#         print(f'Id : {obj.id}\tItemName : {obj.name}\tPrice : {obj.price}')
-#         print('----------------------------------------------------')
-#         total += obj.price
-#     print(f'\t\tTotal : {total}')
-#     print('\tThanks for visiting')
-#     print('\n\n')
-#
-#
-# # Main program
-# customer_name, customer_address = take_customer_info()
-# item_list = take_item_list()
-# display_bill(item_list, customer_name, customer_address)
-
-item_name = []
-
+def bill():
+    temp = {}
+    while True:
+        item_no = int(input('Item_no: '))
+        quantity = int(input('quantity'))
+        temp[item_no] = quantity
+        print("do you want to enter more items y/n?".upper())
+        uio = input("-->")
+        if uio.upper() == "N":
+            break
+    return temp
