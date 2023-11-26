81b8b9ee3c6fba72edbaaf76e58dea6489643b6e
Jyotipriya <b8777051595@gmail.com>
2023-11-27 02:40:37 +0530

    .

--- bill_window.py
+++ bill_window.py
@@ -1,11 +1,21 @@
-def bill():
+# <<<<<<< HEAD
+def bill(typ=0):
     temp = {}
+    temp_lt = []
     while True:
         item_no = int(input('Item_no: '))
         quantity = int(input('quantity'))
+        temp_lt.append((item_no, quantity))
         temp[item_no] = quantity
         print("do you want to enter more items y/n?".upper())
         uio = input("-->")
         if uio.upper() == "N":
             break
-    return temp
+    # return temp
+    if typ:
+        return temp_lt
+    elif not typ:
+        return temp
+    else:
+        return
+
